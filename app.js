bf9c6907b76448046c6a6255ec1db97e6c2fbb7c
Vithujan99 <vithujan99@hotmail.com>
2022-07-11 13:26:56 +0200

    Level5 Using Passport.js to add Cookies and Sessions

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
+
+app.use(session({
+    secret: "Our little Secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 
@@ -20,7 +30,14 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -28,23 +45,21 @@ app.get("/", function(req, res){
 
 app.route("/register")
     .get(function(req, res){
-        res.render("register");
+        if(req.isAuthenticated()){
+            res.redirect("/secrets");
+        }else{
+            res.render("register");
+        }
     })
     .post(function(req, res){
-        User.exists({email: req.body.username}, function(err, foundUser){
-            if(foundUser){
-                res.send("User already exists");
+        
+        User.register({username: req.body.username}, req.body.password, function(err, user){
+            if(err){
+                console.log(err);
+                res.redirect("/register")
             }else{
-                const user = new User({
-                    email: req.body.username,
-                    password: md5(req.body.password)
-                })
-                user.save(function(err){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        res.render("secrets");
-                    }
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
                 });
             }
         });
@@ -53,25 +68,46 @@ app.route("/register")
 
 app.route("/login")
     .get(function(req, res){
-        res.render("login");
+        if(req.isAuthenticated()){
+            res.redirect("/secrets");
+        }else{
+            res.render("login");
+        }
     })
     .post(function(req, res){
-        User.findOne({email: req.body.username}, function(err, foundUser){
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+        req.login(user, function(err){
             if(err){
                 console.log(err);
+                res.redirect("/login");
             }else{
-                if(foundUser){
-                    if(foundUser.password === md5(req.body.password)){
-                        res.render("secrets");
-                    }else{
-                        res.send("Wrong Password")
-                    }
-                }else{
-                    res.send("User not found")
-                }              
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
+                });
             }
-        })
+        });
     });
+
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    }else{
+        res.redirect("/login")
+    }
+});
+
+app.get("/logout", function(req, res){
+    req.logout(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/");
+        }
+    });
+});
 
 //No app.get for secrets and submit they should be only reacheble trough login adn register!!
 app.listen(3000, function(){
